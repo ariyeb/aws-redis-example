9d2c9f34b44c3a199482b07e9a640a8729063d56
Ariye Bar-tov <ariyeb@gmail.com>
2021-03-16 21:35:24 +0200

    redis-router

--- src/routers/redisRouter.js
+++ src/routers/redisRouter.js
@@ -4,19 +4,68 @@ const redisClient = require('../db/redis');
 
 const router = new express.Router();
 
-// const redisClient = require('../db/redis');
+router.post("/set-string", async (req, res) => {
+    try {
+        await redisClient.setAsync(req.body.key, req.body.value);
 
-// const setString = async (req, res, next) => {
-//     try {
-//         const data = await redisClient.setAsync(req.body.key, req.body.value);
-//         next();
-//     } catch (err) {
-//         console.log(err);
-//     }
-// };
+        res.send();
+    } catch (err) {
+        console.log(err);
+    }
+});
 
-// module.exports = {
-//     setString
-// };
+router.get("/get-string/:key", async (req, res) => {
+    try {
+        const value = await redisClient.getAsync(req.params.key);
+
+        res.send({ value });
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+
+router.post("/set-list", async (req, res) => {
+    try {
+        const result = await redisClient.rpushAsync(req.body.key, req.body.list);
+        res.send(result);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get("/get-list/:key", async (req, res) => {
+    try {
+        const list = await redisClient.lrangeAsync(req.params.key, 0, -1);
+        console.log(list);
+        res.send(list);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.post("/set-hash", async (req, res) => {
+    const hashArray = [];
+    console.log(req.body.hash.field1);
+    for (let [k, v] of Object.entries(req.body.hash)) {
+        hashArray.push(k);
+        hashArray.push(v);
+    }
+    try {
+        const data = await redisClient.hmsetAsync(req.body.key, hashArray);
+        res.send(data);
+    } catch (err) {
+        console.log(err);
+    }
+});
+
+router.get("/get-hash/:key", async (req, res) => {
+    try {
+        const hash = await redisClient.hgetallAsync(req.params.key);
+        res.send(hash);
+    } catch (err) {
+        console.log(err);
+    }
+});
 
 module.exports = router;
